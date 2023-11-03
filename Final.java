698b903660ee9fc5e235bb8ccbacf819d09297de
Z3rks <santiago.p1290@gmail.com>
2023-11-03 07:44:52 -0500

    commit2

--- Final.java
+++ Final.java
@@ -3,7 +3,23 @@ import java.io.*;
 public class Final {
 
     public static void main(String[] args) {
-        File archivo = 
+
+        String[][] lunesDatos = new String[100][5];
+
+        for (int i = 0; i < 100; i++) {
+            File archivo = null;
+            BufferedReader br = null;
+
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("Lunes.txt"));
+            for (){
+                
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
     }
 }
