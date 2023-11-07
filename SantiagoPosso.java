d04c5b8d5e80d40f9946f05c5e9ff40adc62b71b
Z3rks <santiago.p1290@gmail.com>
2023-11-06 19:11:13 -0500

    commit3

---
+++ SantiagoPosso.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.*;
+
+
+public class SantiagoPosso {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] dias = {"lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"};
+        String[][] data = new String[7][100];
+
+        for (int i = 0; i < 7; i++) {
+            String dia = dias[i];
+            String fileName = "C:\\Users\\user\\OneDrive\\Escritorio\\Santiago Posso\\" + dia + ".txt";
+          
+        }// fin del for
+
+        
+
+       
+    }// fin main
+}// fin class
